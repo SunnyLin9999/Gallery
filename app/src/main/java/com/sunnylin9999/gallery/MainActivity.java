e4202b2d50c487d2430734663ba014fc6881297a
SunnyLin9999 <sunnylin9999@gmail.com>
2020-04-29 20:42:17 +0800

    Add Storage Permission using EasyPermissions Library
    1. 使用 EasyPermissions Library (https://github.com/googlesamples/easypermissions)
    2. 檢查READ_EXTERNAL_STORAGE權限
    3. 權限被拒絕之後，再開啟APP => 顯示rationale字串(R.string.rationale_storage)
    4. 勾選"NEVER ASK AGAIN." => 跳出APP設定對話框(AppSettingsDialog)，顯示系統的Premissions Required和Toast訊息 => 須由App Info手動開權限

--- app/src/main/java/com/sunnylin9999/gallery/MainActivity.java
+++ app/src/main/java/com/sunnylin9999/gallery/MainActivity.java
@@ -1,14 +1,28 @@
 package com.sunnylin9999.gallery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+    private String TAG = "MainActivity";
+
+    private static final String[] READ_STORGE =
+            {Manifest.permission.READ_EXTERNAL_STORAGE};
+
+    private static final int RC_STORAGE_PERM = 1000;
 
     private GridView gridView;
     private int[] imageList = {R.drawable.ic_launcher_background, R.drawable.ic_launcher_background, R.drawable.ic_launcher_background,
@@ -24,6 +38,81 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         gridView = (GridView) findViewById(R.id.gridview);
+    }
+
+    private boolean hasStoragePermission() {
+        return EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        // Some permissions have been granted
+        Log.d(TAG, "onPermissionsGranted:" + requestCode + ":" + perms.size());
+
+        if (requestCode == RC_STORAGE_PERM) {
+            showPhotos();
+        }
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        // Some permissions have been denied
+        Log.d(TAG, "onPermissionsDenied:" + requestCode + ":" + perms.size());
+
+        // (Optional) Check whether the user denied any permissions and checked "NEVER ASK AGAIN."
+        // This will display a dialog directing them to enable the permission in app settings.
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
+            String yes = getString(R.string.yes);
+            String no = getString(R.string.no);
+
+            // Do something after user returned from app settings screen, like showing a Toast.
+            Toast.makeText(
+                    this,
+                    getString(R.string.returned_from_app_settings_to_activity,
+                            hasStoragePermission() ? yes : no),
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+	
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(hasStoragePermission()) {
+            Toast.makeText(this, "TODO: Read Storage things", Toast.LENGTH_LONG).show();
+            showPhotos();
+        } else {
+            //Ask for one permission
+            EasyPermissions.requestPermissions(
+                    this,
+                    getString(R.string.rationale_storage),
+                    RC_STORAGE_PERM,
+                    READ_STORGE
+            );
+        }
+    }
+
+    private void showPhotos() {
         GridAdapter adapter = new GridAdapter(MainActivity.this, imageList, titleList);
         gridView.setAdapter(adapter);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
