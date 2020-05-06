bc8872d2264ac6a24b282b9712a25d46f9ff884a
SunnyLin9999 <sunnylin9999@gmail.com>
2020-05-06 19:47:40 +0800

    Modify Fragment Load Photo Method Move to AndroidViewModel
    1. 將取得圖片的功能，從Fragment移至ViewModel，使用可被觀測的LiveData存資料 => 以符合單一職責設計原則
    2. 使用ViewModel，管理UI資料 => 解決旋轉螢幕時資料回收再重啟的問題
    3. Fragment註冊Observer觀察ViewModel中LiveData資料更新，當Fragment的lifecycle狀態到Destory，自動解除對LiveData的監聽 => 避免Memory Leak
    4. AndroidViewModel(AVM) has application context. => 可使用Fragment的context

--- app/src/main/java/com/sunnylin9999/gallery/ui/home/HomeFragment.java
+++ app/src/main/java/com/sunnylin9999/gallery/ui/home/HomeFragment.java
@@ -3,10 +3,8 @@ package com.sunnylin9999.gallery.ui.home;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -14,8 +12,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,7 +26,6 @@ import com.sunnylin9999.gallery.R;
 import com.sunnylin9999.gallery.model.PhotoItem;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
@@ -49,22 +44,44 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
-        // TODO: Use the ViewModel
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        gridView = root.findViewById(R.id.grid_home);
+
+        context = getActivity();
+
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+
+        Observer<String> textObserver = new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        };
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textObserver);
 
-        context = getContext();
+        Observer<List<PhotoItem>> loadPhotosObserver = new Observer<List<PhotoItem>>() {
+            @Override
+            public void onChanged(@Nullable final List<PhotoItem> photoItems) {
+                GridAdapter adapter = new GridAdapter(context, photoItems);
+                gridView.setAdapter(adapter);
+                gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        PhotoItem item = photoItems.get(position);
+                        Toast.makeText(context, "Name: \"" + item.getPhotoName() + "\"" +
+                                ", \n\nLocation: \"" + item.getPhotoPath() + "\"" , Toast.LENGTH_SHORT).show();
 
-
-        gridView = (GridView) root.findViewById(R.id.grid_home);
-
+                        Bundle bundle = new Bundle();
+                        bundle.putString("imageUri", String.valueOf(Uri.fromFile(new File(item.getPhotoPath()))));
+                        Intent intent = new Intent(context, PhotoViewActivity.class);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    }
+                });
+            }
+        };
+        homeViewModel.loadAllPhotos().observe(getViewLifecycleOwner(), loadPhotosObserver);
 
         return root;
     }
@@ -72,63 +89,5 @@ public class HomeFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        showPhotos();
     }
-
-    private void showPhotos() {
-        final List<PhotoItem> photoItems = getAllPhotos();
-
-        GridAdapter adapter = new GridAdapter(context, photoItems);
-        gridView.setAdapter(adapter);
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                PhotoItem item = photoItems.get(position);
-                Toast.makeText(context, "Name: \"" + item.getPhotoName() + "\"" +
-                        ", \n\nLocation: \"" + item.getPhotoPath() + "\"" , Toast.LENGTH_SHORT).show();
-
-                Bundle bundle = new Bundle();
-                bundle.putString("imageUri", String.valueOf(Uri.fromFile(new File(item.getPhotoPath()))));
-                Intent intent = new Intent(context, PhotoViewActivity.class);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private List<PhotoItem> getAllPhotos() {
-        List<PhotoItem> photoItems= new ArrayList<>();
-
-        ContentResolver contentResolver = context.getContentResolver();
-
-        String[] projection = new String[] {
-                MediaStore.Images.ImageColumns._ID,
-                MediaStore.Images.ImageColumns.DISPLAY_NAME,
-                MediaStore.Images.ImageColumns.DATA,
-                MediaStore.Images.ImageColumns.DATE_ADDED,
-        };
-
-        Cursor cursor = contentResolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                projection,
-                null,
-                null,
-                MediaStore.Images.ImageColumns.DATE_MODIFIED + "  desc");
-
-        while (cursor.moveToNext()) {
-            String photoId = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns._ID));
-            String photoName = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DISPLAY_NAME));
-            String photoPath = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA));
-            String photoDateAdded = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATE_ADDED));
-            Log.d(TAG, photoId + ", " + photoName + ", " + photoPath + ", " + photoPath);
-
-            PhotoItem photoItem = new PhotoItem(photoId, photoPath, photoName, photoDateAdded);
-            photoItems.add(photoItem);
-        }
-        cursor.close();
-        cursor = null;
-
-        return photoItems;
-    }
-
 }
--- app/src/main/java/com/sunnylin9999/gallery/ui/home/HomeViewModel.java
+++ app/src/main/java/com/sunnylin9999/gallery/ui/home/HomeViewModel.java
@@ -1,23 +1,77 @@
 package com.sunnylin9999.gallery.ui.home;
 
-import androidx.lifecycle.LiveData;
+import android.app.Application;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.MediaStore;
+import android.util.Log;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
 import com.sunnylin9999.gallery.model.PhotoItem;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HomeViewModel extends ViewModel {
+public class HomeViewModel extends AndroidViewModel {
+    private String TAG = "HomeViewModel";
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+    private MutableLiveData<List<PhotoItem>> mPhotoItems;
+
+    public HomeViewModel(Application application) {
+        super(application);
     }
 
-    public LiveData<String> getText() {
+    public MutableLiveData<String> getText() {
+        if(mText == null) {
+            mText = new MutableLiveData<>();
+            mText.setValue("This is home fragment");
+        }
         return mText;
     }
 
 
+    public MutableLiveData<List<PhotoItem>> loadAllPhotos() {
+        if(mPhotoItems == null) {
+            mPhotoItems = new MutableLiveData<>();
+        }
+
+        List<PhotoItem> items = new ArrayList<>();
+
+        ContentResolver contentResolver = getApplication().getContentResolver();
+
+        String[] projection = new String[] {
+                MediaStore.Images.ImageColumns._ID,
+                MediaStore.Images.ImageColumns.DISPLAY_NAME,
+                MediaStore.Images.ImageColumns.DATA,
+                MediaStore.Images.ImageColumns.DATE_ADDED,
+        };
+
+        Cursor cursor = contentResolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                null,
+                null,
+                MediaStore.Images.ImageColumns.DATE_MODIFIED + "  desc");
+
+        while (cursor.moveToNext()) {
+            String photoId = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns._ID));
+            String photoName = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DISPLAY_NAME));
+            String photoPath = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA));
+            String photoDateAdded = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATE_ADDED));
+            Log.d(TAG, photoId + ", " + photoName + ", " + photoPath + ", " + photoPath);
+
+            PhotoItem photoItem = new PhotoItem(photoId, photoPath, photoName, photoDateAdded);
+            items.add(photoItem);
+        }
+        cursor.close();
+        cursor = null;
+
+//        for (PhotoItem item: items) {
+//            Log.i(TAG, item.getPhotoId() + ", " + item.getPhotoName());
+//        }
+
+        mPhotoItems.setValue(items);
+        return mPhotoItems;
+    }
 }
